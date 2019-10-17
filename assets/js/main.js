a138fe7ab4bb825dc8634b8b5eb675e2644c4497
Francesco <franc.ns@gmail.com>
2019-10-17 15:08:15 +0200

    added discount message

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,8 +14,10 @@ price = (km * 0.21);
 // verifica condizioni per eventuale sconto e calcolo sconto
 if (age < 18) {
   discount = ((price / 100) * 20);
+  document.getElementById('js-discount-title').innerHTML = ('Sconto 20% under 18:');
 } else if (age > 65) {
   discount = ((price / 100) * 40);
+  document.getElementById('js-discount-title').innerHTML = ('Sconto 40% over 65:');
 } else {
   discount = 0;
 }
